93e8d7ce111887245bd943519bbf874a112744b0
Pmant <5005155+Pmant@users.noreply.github.com>
2026-08-08 15:49:33 +0200

    change concurrency limits

--- src/server/concurrencyLimit.ts
+++ src/server/concurrencyLimit.ts
@@ -1,7 +1,7 @@
 import { getAllMatches, getAllLobbies } from './store';
 
-const MAX_LOBBIES = 100;
-const MAX_MATCHES = 100;
+const MAX_LOBBIES = 10001;
+const MAX_MATCHES = 10001;
 
 export function canCreateLobby(): boolean {
   return getAllLobbies().size < MAX_LOBBIES;
